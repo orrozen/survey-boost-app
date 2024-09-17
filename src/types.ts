ccb20f710a8276376099559fc681f985eba420dc
or <orro@wix.com>
2024-09-17 12:16:35 +0300

    data models #pr

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,25 @@
-export type Settings = {
-  amount: number;
-  title: string;
-  color: string;
-  iconColor: string;
-};
+export type Survey = {
+  question1: string | undefined;
+  question2: string | undefined;
+  question3: string | undefined;
+  coupon: Coupon;
+}
+
+export interface Coupon {
+  type: CouponType
+  // To be used for percent off and money off
+  amount: number
+}
+
+export enum CouponType {
+  FREE_SHIPPING,
+  MONEY_OFF_AMOUNT,
+  PERCENT_OFF_AMOUNT,
+}
+
+export type Submissions = {
+  purchaseFlowId: string;
+  answer1: string | undefined;
+  answer2: string | undefined;
+  answer3: string | undefined;
+}
